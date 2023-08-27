3a20472bf38c6ece7c9dc8934899b1774b292a5a
Amim7925 <amim.464432@gmail.com>
2023-08-27 11:16:31 +0330

    Feature : initial migration was passed to database , added appsetting.json file for connection string , added required packages for interacting with json file and sql server

--- FreightAccounting.Core/FreightAccountingContext.cs
+++ FreightAccounting.Core/FreightAccountingContext.cs
@@ -1,14 +1,32 @@
 ﻿using FreightAccounting.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FreightAccounting.Core;
 
 public class FreightAccountingContext : DbContext
 {
+    public FreightAccountingContext()
+    {
+        
+    }
+
     public FreightAccountingContext(DbContextOptions<FreightAccountingContext> options) 
         : base(options)
     {
-        
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+              .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+        string connectionString = configuration.GetConnectionString("SqlConnectionString") ?? string.Empty;
+
+        optionsBuilder.UseSqlServer(connectionString);
     }
 
     public DbSet<Remittance> Remittances { get; set; }
