cde2be98d54156268c036d52de07c2c9eef312e9
mohamad <NGMPC3@PCALIREZA>
2023-08-27 10:17:13 +0330

    feature: install material design theme and code , install persian date picker and persian date nuget package, initial design for header main window

--- FreightAccounting.WPF/MainWindow.xaml.cs
+++ FreightAccounting.WPF/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,94 @@ namespace FreightAccounting.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void gridHears_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnChangeTheme_Checked(object sender, RoutedEventArgs e)
+        {
+            var paletteHelper = new PaletteHelper();
+            //Retrieve the app's existing theme
+            ITheme theme = paletteHelper.GetTheme();
+
+            //Change the base theme to Dark
+            theme.SetBaseTheme(Theme.Dark);
+            //or theme.SetBaseTheme(Theme.Light);
+
+            //Change all of the primary colors to Red
+            theme.SetPrimaryColor(Colors.Blue);
+
+            //Change all of the secondary colors to Blue
+            theme.SetPrimaryColor(SwatchHelper.Lookup[(MaterialDesignColor)PrimaryColor.LightBlue]);
+
+            ////You can also change a single color on the theme, and optionally set the corresponding foreground color
+            //theme.PrimaryMid = new ColorPair(Colors.Brown, Colors.White);
+
+            //Change the app's current theme
+            paletteHelper.SetTheme(theme);
+
+            //todo -- save to app setting for when user open after change theme -- todo
+        }
+
+        private void btnChangeTheme_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var paletteHelper = new PaletteHelper();
+            //Retrieve the app's existing theme
+            ITheme theme = paletteHelper.GetTheme();
+
+            //Change the base theme to Dark
+            theme.SetBaseTheme(Theme.Light);
+            //or theme.SetBaseTheme(Theme.Light);
+
+            //Change all of the primary colors to Red
+            theme.SetPrimaryColor(SwatchHelper.Lookup[(MaterialDesignColor)PrimaryColor.LightBlue]);
+
+            ////Change all of the secondary colors to Blue
+            //theme.SetSecondaryColor(Colors.Blue);
+
+            ////You can also change a single color on the theme, and optionally set the corresponding foreground color
+            //theme.PrimaryMid = new ColorPair(Colors.Brown, Colors.White);
+
+            //Change the app's current theme
+            paletteHelper.SetTheme(theme);
+
+            //todo -- save to app setting for when user open after change theme -- todo
+        }
+
+        private void btnChangePassword_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
